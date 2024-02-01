b65d92fa379e9a1fc3afcdf8287886ab465e3b06
David <dmg_92@hotmail.com>
2024-01-31 19:02:23 -0600

    ejemplo token

--- src/components/apis/regions.jsx
+++ src/components/apis/regions.jsx
@@ -3,13 +3,15 @@ import axios from 'axios';
 const entertainmentStreamingUrl = process.env.REACT_APP_API_URL;
 const apiUrl = `${entertainmentStreamingUrl}/api/regions/`;
 
-console.log('apiUrl', apiUrl)
 
 export const getRegionsList = async () => {
+  const user = JSON.parse(localStorage.getItem("user"))
+
   try {
     const response = await axios.get(apiUrl, {
       headers: {
         "Content-Type": "application/json",
+        "Authorization": `Token ${user.token}`,
       },
     });
     console.log(response.data)
